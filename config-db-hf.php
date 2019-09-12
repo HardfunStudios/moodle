faaa33259afc37bdf25de07e681c7294a4cbdadf
Augusto Bennemann <gutobenn@gmail.com>
2019-09-12 19:49:39 -0300

    Sincroniza campos de perfil a partir da LP

---
+++ config-db-hf.php
@@ -0,0 +1,19 @@
+<?php
+
+$LP_DB_CFG = [
+    'dbtype'    => 'pgsql',
+    'dbhost'    => 'lp.local',
+    'dbname'    => 'ecab_development',
+    'dbuser'    => 'augusto',
+    'dbpass'    => ''
+];
+
+$MOODLE_DB_CFG = [
+    'dbtype'    => 'pgsql',
+    'dbhost'    => 'localhost',
+    'dbname'    => 'moodle35',
+    'dbuser'    => 'postgres',
+    'dbpass'    => 'password',
+    'prefix'    => 'mdl_'
+];
+
