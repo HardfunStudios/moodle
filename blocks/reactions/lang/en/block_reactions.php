2eddc68306827940fa7eb8adce72830be14ceae9
rodrigorichter <guigorichter@gmail.com>
2020-06-29 15:46:05 -0300

    arrumado outros arquivos estarem no repositorio do moodle ao invés do bloco

--- blocks/reactions/lang/en/block_reactions.php
+++
@@ -1,5 +0,0 @@
-<?php
-$string['pluginname'] = 'Reactions Block';
-$string['reactions'] = 'Reactions';
-$string['reactions:addinstance'] = 'Add a new reactions block';
-$string['reactions:myaddinstance'] = 'Add a new reactions block to the My Moodle page';
